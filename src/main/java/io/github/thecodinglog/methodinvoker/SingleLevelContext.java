f1a6c493177e0cc55f2047094b6163eb39e3e825
Jeongjin Kim <thefuturecreator@gmail.com>
2022-07-04 17:10:37 +0900

    refactor.

--- src/main/java/io/github/thecodinglog/methodinvoker/SingleLevelContext.java
+++ src/main/java/io/github/thecodinglog/methodinvoker/SingleLevelContext.java
@@ -1,7 +1,6 @@
 package io.github.thecodinglog.methodinvoker;
 
 import io.github.thecodinglog.methodinvoker.exceptions.NoUniqueElementException;
-import org.springframework.core.ResolvableType;
 
 import java.lang.reflect.Type;
 import java.util.*;
@@ -33,10 +32,9 @@ public class SingleLevelContext implements Context {
     }
 
     private List<String> extractKeysByType(Type type) {
-        ResolvableType resolvableType = ResolvableType.forType(type);
         List<String> keys = new ArrayList<>(store.entrySet().size());
         for (Map.Entry<String, TypeDescribableObject> entry : store.entrySet()) {
-            if (resolvableType.isAssignableFrom(ResolvableType.forType(entry.getValue().getType())))
+            if (TypeUtils.isAssignable(type, entry.getValue().getType()))
                 keys.add(entry.getKey());
         }
         return keys;
